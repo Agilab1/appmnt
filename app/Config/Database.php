1b6f19fb565c4d2d0f853af9f77876413337af9c
Aditya Kamble <127715039+AdityaKamble11@users.noreply.github.com>
2026-02-11 17:49:58 +0530

    database updated

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,41 +27,11 @@ class Database extends Config
 
     // local MySQL database credentials -------------------------------------------------------------//
 
-    public array $default = [
-        'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => 'root',
-        'password'     => '',
-        'database'     => 'agiletwn_appmnt',
-        'DBDriver'     => 'MySQLi',
-        'DBPrefix'     => '',
-        'pConnect'     => false,
-        'DBDebug'      => true,
-        'charset'      => 'utf8mb4',
-        'DBCollat'     => 'utf8mb4_general_ci',
-        'swapPre'      => '',
-        'encrypt'      => false,
-        'compress'     => false,
-        'strictOn'     => false,
-        'failover'     => [],
-        'port'         => 3306,
-        'numberNative' => false,
-        'foundRows'    => false,
-        'dateFormat'   => [
-            'date'     => 'Y-m-d',
-            'datetime' => 'Y-m-d H:i:s',
-            'time'     => 'H:i:s',
-        ],
-    ];
-
-
-    // cpanel MySQL database credentials -------------------------------------------------------------//
-
     // public array $default = [
     //     'DSN'          => '',
-    //     'hostname'     => 'agilesaptech.com',
-    //     'username'     => 'agiletwn_appmnt',
-    //     'password'     => 'agiletwn_appmnt',
+    //     'hostname'     => 'localhost',
+    //     'username'     => 'root',
+    //     'password'     => '',
     //     'database'     => 'agiletwn_appmnt',
     //     'DBDriver'     => 'MySQLi',
     //     'DBPrefix'     => '',
@@ -83,6 +53,36 @@ class Database extends Config
     //         'time'     => 'H:i:s',
     //     ],
     // ];
+
+
+    // cpanel MySQL database credentials -------------------------------------------------------------//
+
+    public array $default = [
+        'DSN'          => '',
+        'hostname'     => 'agilesaptech.com',
+        'username'     => 'agiletwn_appmnt',
+        'password'     => 'agiletwn_appmnt',
+        'database'     => 'agiletwn_appmnt',
+        'DBDriver'     => 'MySQLi',
+        'DBPrefix'     => '',
+        'pConnect'     => false,
+        'DBDebug'      => true,
+        'charset'      => 'utf8mb4',
+        'DBCollat'     => 'utf8mb4_general_ci',
+        'swapPre'      => '',
+        'encrypt'      => false,
+        'compress'     => false,
+        'strictOn'     => false,
+        'failover'     => [],
+        'port'         => 3306,
+        'numberNative' => false,
+        'foundRows'    => false,
+        'dateFormat'   => [
+            'date'     => 'Y-m-d',
+            'datetime' => 'Y-m-d H:i:s',
+            'time'     => 'H:i:s',
+        ],
+    ];
 
 
     //    /**
