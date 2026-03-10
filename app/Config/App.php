7dc6d50bf1005f72b97fe4e2f6f1f7f1872767c5
Aditya Kamble <127715039+AdityaKamble11@users.noreply.github.com>
2026-03-10 17:19:14 +0530

    App link

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,8 +16,8 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/appmnt/';
-    // public string $baseURL = 'https://agilesaptech.com/appmnt/';
+    // public string $baseURL = 'http://localhost/appmnt/';
+    public string $baseURL = 'https://agilesaptech.com/appmnt/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
